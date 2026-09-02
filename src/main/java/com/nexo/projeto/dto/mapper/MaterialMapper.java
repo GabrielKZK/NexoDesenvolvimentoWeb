404825e5491deb451abcae3bcf20b1d3fe92b265
LUCAS MACHADO FLORIANO <lucas_m_floriano@estudante.sesisenai.org.br>
2026-09-01 21:37:28 -0300

    entity, dto e repository concluidos, mapper quase finalizado

---
+++ src/main/java/com/nexo/projeto/dto/mapper/MaterialMapper.java
@@ -0,0 +1,29 @@
+package com.nexo.projeto.dto.mapper;
+
+import com.nexo.projeto.dto.MateriaDto;
+import com.nexo.projeto.entity.Materia;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MaterialMapper {
+    public MateriaDto toDto(Materia m){
+
+        return new MateriaDto(
+                m.getId(),
+                m.getNome(),
+                m.getSegmento())
+                ;
+    }
+
+    public Materia toEntity(MateriaDto dto) {
+        if (dto == null)
+            return null;
+
+        Materia m = new Materia();
+        m.setId(dto.id());
+        m.setNome(dto.nome());
+        m.setSegmento(dto.segmento());
+
+        return m;
+    }
+}
